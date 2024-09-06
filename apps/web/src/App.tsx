df33e89fae7853635ca02a67dce876736ba64a0f
Manuel Bogino <manuelbogino@gmail.com>
2024-09-06 00:15:35 -0300

    Add base web project

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,13 @@
+import { Typography } from "@mui/material";
+
+function App() {
+	return (
+		<>
+			<header>
+				<Typography variant="h1">SHOPPING LIST</Typography>
+			</header>
+		</>
+	);
+}
+
+export default App;
---
+++ apps/web/src/theme.ts
@@ -0,0 +1,40 @@
+import { createTheme } from "@mui/material/styles";
+import { red } from "@mui/material/colors";
+
+// A custom theme for this app
+const theme = createTheme({
+	typography: {
+		h1: {
+			fontFamily: "Dosis, sans-serif",
+			fontSize: 18,
+			fontWeight: 600,
+			lineHeight: "22px",
+		},
+		h2: {
+			fontFamily: "Dosis, sans-serif",
+		},
+		h3: {
+			fontFamily: "Dosis, sans-serif",
+		},
+		h4: {
+			fontFamily: "Dosis, sans-serif",
+		},
+		h5: {
+			fontFamily: "Dosis, sans-serif",
+		},
+		fontFamily: "Nunito, sans-serif",
+	},
+	palette: {
+		primary: {
+			main: "#556cd6",
+		},
+		secondary: {
+			main: "#19857b",
+		},
+		error: {
+			main: red.A400,
+		},
+	},
+});
+
+export default theme;
